3adc99dab474a8e4381059be63cbe8bf7251f831
bruno felipe <bruno.f.martins1996@gmail.com>
2022-04-14 15:36:50 -0300

    Atualizando AlunoService

--- src/main/java/com/academy/gym/services/AlunoService.java
+++ src/main/java/com/academy/gym/services/AlunoService.java
@@ -1,16 +1,8 @@
-/*package com.academy.gym.services;
+package com.academy.gym.services;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-import javax.transaction.Transactional;
-
-import com.academy.gym.models.Aluno;
 import com.academy.gym.repositories.AlunoRepository;
 
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class AlunoService {
@@ -20,27 +12,4 @@ public class AlunoService {
     public AlunoService(AlunoRepository alunoRepository){
         this.AlunoRepository = alunoRepository;
     }
-
-    @Transactional
-    public Aluno save(Aluno aluno){
-       return this.AlunoRepository.save(aluno);
-    }
-    public List<Aluno>getAll(){
-        return this.AlunoRepository.findAll();
-    }
-    public Aluno getById(Aluno aluno){
-       return this.AlunoRepository.getById(aluno.getId());
-    }
-    public Optional<Aluno> findById(UUID id) {
-        return this.AlunoRepository.findById(id);
-    }
-    public void update(UUID id){
-        Optional<Aluno> aluno = this.AlunoRepository.findById(id);
-        this.AlunoRepository.save(aluno);
-    }
-    public void delete(UUID id){
-        Optional<Aluno> aluno = this.AlunoRepository.findById(id);
-        this.AlunoRepository.delete(aluno);
-    }
-    
-}*/
+}
--- src/main/java/com/academy/gym/services/FichaService.java
+++ src/main/java/com/academy/gym/services/FichaService.java
@@ -12,7 +12,4 @@ public class FichaService {
     public FichaService(FichaRepository fichaRepository) {
         this.fichaRepository = fichaRepository;
     }
-
-    
-    
 }
